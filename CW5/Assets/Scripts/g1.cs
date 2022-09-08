257fb632f39d44ecc23c9f663f3db03283befba7
alialaradah <100695337+alialaradah@users.noreply.github.com>
2022-09-08 19:35:53 +0300

    FP

---
+++ CW5/Assets/Scripts/g1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class g1 : MonoBehaviour
+{
+
+    bool btn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void gg()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void nnn()
+    {
+        SceneManager.LoadScene("1.");
+    }
+}
